6bbbf866d8d4342969de3f354e6d731e90076b64
Jonas Kuß <jonaskuss@protonmail.com>
2021-11-23 17:12:43 +0100

    Funktionierender Code von Johannes mit eigenen GPIO Pin-defines

--- src/SPI_communication_test.cpp
+++ src/SPI_communication_test.cpp
@@ -1,44 +1,60 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <stdio.h>
 
-// GPIO pins für RSTn, SPICSn und IRQ
-#define LoRa_CHIPSELECT 8
+// GPIO pins für RSTn und SPICSn
 #define DWM3000_RESET 10
 #define DWM3000_CHIPSELECT 11
 
-#define DWM3000_RESET_WAIT_uS 1000
-#define DWM3000_RESET_LOW_WAIT_uS 100
+SPISettings DWM3000_SPI_SETTINGS = SPISettings(10000000, MSBFIRST, SPI_MODE0);
+
+#define SERIAL_CONNECT_WAIT 5000
 #define CMD_RESEND_WAIT 2000
+#define LORA_CS 8
 
-void setup_DWM3000() {
-  // damit der LoRa CS nicht stört
-  pinMode(LoRa_CHIPSELECT, OUTPUT);
-  digitalWrite(LoRa_CHIPSELECT, HIGH);
+uint8_t dev_id[4];
 
-  pinMode(DWM3000_RESET, OUTPUT);
-  digitalWrite(DWM3000_RESET, LOW);
-  delayMicroseconds(DWM3000_RESET_LOW_WAIT_uS);
-  pinMode(DWM3000_RESET, INPUT);
-  delayMicroseconds(DWM3000_RESET_WAIT_uS);
+void reset_DWM3000() {
+    pinMode(DWM3000_RESET, OUTPUT);
+    digitalWrite(DWM3000_RESET, LOW);
+    delayMicroseconds(100);
+    pinMode(DWM3000_RESET, INPUT);
+    delay(1); //give the DWM3000 chip time to come up
 }
 
-uint8_t DW3000_get_device_id() {
-  digitalWrite(DWM3000_CHIPSELECT, LOW);
-  SPI.transfer(0);
-  uint8_t id = SPI.transfer(0);
-  digitalWrite(DWM3000_CHIPSELECT, HIGH);
-  return id;
+void get_device_id(uint8_t dev_id[]) {
+    SPI.beginTransaction(DWM3000_SPI_SETTINGS);
+    digitalWrite(DWM3000_CHIPSELECT, LOW);
+
+    //read device id
+    SPI.transfer(0x00);
+    for(int i=0; i<4; i++){
+        dev_id[i] = SPI.transfer(0x00);
+    }
+
+    digitalWrite(DWM3000_CHIPSELECT, HIGH);
+    SPI.endTransaction();
 }
 
 void setup() {
-  setup_DWM3000();
-  SPI.begin();
+    delay(SERIAL_CONNECT_WAIT);
+    reset_DWM3000();
+    pinMode(DWM3000_CHIPSELECT, OUTPUT);
+
+    pinMode(LORA_CS, OUTPUT);
+    digitalWrite(LORA_CS, HIGH);
+
+    SPI.begin();
 }
 
 void loop() {
-  uint8_t dev_id = DW3000_get_device_id();
-  Serial.print("Device ID = ");
-  Serial.println(dev_id);
-  delay(CMD_RESEND_WAIT);
+    get_device_id(dev_id);
+    Serial.print("Device ID = ");
+    //print device id in reverse order
+    for(int i=0; i<4; i++){
+        Serial.print(dev_id[3-i], HEX);
+        Serial.print(" ");
+    }
+
+    Serial.println();
+    delay(CMD_RESEND_WAIT);
 }
