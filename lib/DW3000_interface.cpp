ab62502a6af9db12e1bb368339e71955a9d300cc
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-01 23:10:27 +0100

    DW3000 initialisieren und für die Annahme/Senden von Nachrichten konfigurieren.

--- lib/DW3000_interface.cpp
+++ lib/DW3000_interface.cpp
@@ -2,6 +2,34 @@
 #include <lib/DW3000_DECA_API/Src/decadriver/deca_regs.h>
 #include <Arduino.h>
 
+DW3000_Interface::DW3000_Interface() {
+  m_spi_settings = SPISettings(10000000, MSBFIRST, SPI_MODE0);
+  reset_DW3000();
+  dwt_initialise(DWT_DW_INIT);
+
+  // TODO: nachfragen ob diese Werte in Ordnung sind
+  dwt_config_t config = {
+    .chan = 5,
+    .txPreambLength = DWT_PLEN_32,
+    .rxPAC = DWT_PAC4,
+    .txCode = 3,
+    .rxCode = 3,
+    .sfdType = DWT_SFD_DW_16,
+    .dataRate = DWT_BR_6M8,
+    .phrMode = DWT_PHRMODE_STD,
+    .phrRate = DWT_BR_6M8,
+    .sfdTO = DWT_PLEN_32 + 1 + DWT_SFD_DW_16 - DWT_PAC4,
+    .stsMode = DWT_STS_MODE_OFF,
+    .stsLength = DWT_STS_LEN_32,
+    .pdoaMode = DWT_PDOA_M0,
+  };
+
+  dwt_configure(&config);
+  dwt_setcallbacks(nullptr, receive_packet, nullptr, nullptr, nullptr, nullptr);
+
+  dwt_writefastCMD(CMD_RX);
+}
+
 void DW3000_Interface::reset_DW3000() {
   pinMode(SPI_reset, OUTPUT);
   digitalWrite(SPI_reset, LOW);
