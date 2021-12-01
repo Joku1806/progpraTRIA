59081ed351e13695d1ba441ede5b3fbac62575fe
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-01 22:44:04 +0100

    Einen Haufen Code löschen, weil jetzt die Library von Decawave benutzt wird und deswegen dieses Sammelsurium von defines und Funktionen nicht mehr gebraucht wird.

--- include/lib/DW3000_interface.h
+++ include/lib/DW3000_interface.h
@@ -2,24 +2,13 @@
 
 #include <lib/pin_layout.h>
 #include <lib/TRIA_Packet.h>
-#include <lib/SPI_commands.h>
+#include <lib/DW3000_DECA_API/Src/decadriver/deca_device_api.h>
 #include <SPI.h>
 
 class DW3000_Interface {
-  private:
-    SPISettings DW3000_SPI_Settings;
-    uint8_t get_fast_command_transaction_header(SPI_command cmd);
-    uint8_t get_short_addressed_transaction_header(transaction_type type, uint8_t base);
-    uint16_t get_full_addressed_transaction_header(transaction_type type, uint8_t base, uint8_t sub);
-    uint16_t get_masked_write_transaction_header(uint8_t base, uint8_t sub, transaction_mode mode);
-    
-    void execute_nonreturning_SPI_command(uint8_t cmd);
-    uint64_t DW3000_Interface::get_sys_status();
-
     void reset_DW3000();
     void configure_message_reception();
     void configure_message_transmission();
-
   public:
     DW3000_Interface() {
       DW3000_SPI_Settings = SPISettings(10000000, MSBFIRST, SPI_MODE0);
--- lib/DW3000_interface.cpp
+++ lib/DW3000_interface.cpp
@@ -1,4 +1,5 @@
 #include <lib/DW3000_interface.h>
+#include <lib/DW3000_DECA_API/Src/decadriver/deca_regs.h>
 #include <Arduino.h>
 
 void DW3000_Interface::reset_DW3000() {
@@ -9,49 +10,6 @@ void DW3000_Interface::reset_DW3000() {
   delayMicroseconds(1000);
 }
 
-void DW3000_Interface::execute_nonreturning_SPI_command(uint8_t cmd) {
-  SPI.beginTransaction(this->DW3000_SPI_Settings);
-  digitalWrite(SPI_chipselect, LOW);
-
-  SPI.transfer(cmd);
-
-  digitalWrite(SPI_chipselect, HIGH);
-  SPI.endTransaction();
-}
-
-uint8_t DW3000_Interface::get_fast_command_transaction_header(SPI_command cmd) {
-  return (1 << 7) | cmd & 0x3e | (1 << 0);
-}
-
-uint8_t DW3000_Interface::get_short_addressed_transaction_header(transaction_type type, uint8_t base) {
-  return (type << 7) | base & 0x3e;
-}
-
-uint16_t DW3000_Interface::get_full_addressed_transaction_header(transaction_type type, uint8_t base, uint8_t sub) {
-  return (type << 14) | (base & 0x1f) << 8 | (sub & 0x7f) << 2 | transaction_RD_RW;
-}
-
-uint16_t DW3000_Interface::get_masked_write_transaction_header(uint8_t base, uint8_t sub, transaction_mode mode) {
-  return (3 << 14) | (base & 0x1f) << 8 | (sub & 0x7f) << 2 | mode;
-}
-
-uint64_t DW3000_Interface::get_sys_status() {
-  SPI.beginTransaction(this->DW3000_SPI_Settings);
-  digitalWrite(SPI_chipselect, LOW);
-
-  uint64_t status = 0;
-  uint16_t cmd = get_full_addressed_transaction_header(transaction_read, SYS_STATUS_BASE_REGISTER, SYS_STATUS_SUB_ADDRESS);
-  SPI.transfer16(cmd);
-
-  for (size_t i = 0; i < SYS_STATUS_LENGTH; i++) {
-    status |= SPI.transfer16(cmd) << i * 8;
-  }
-
-  digitalWrite(SPI_chipselect, HIGH);
-  SPI.endTransaction();
-
-  return status;
-}
 
 void DW3000_Interface::configure_message_reception() {
   execute_nonreturning_SPI_command(get_fast_command_transaction_header(CMD_RX));
@@ -75,5 +33,5 @@ void DW3000_Interface::send_packet(TRIA_Packet& packet) {
   while (!(get_sys_status() & TXFRS)) { delayMicroseconds(100); }
   // 2. Paket in TX_BUFFER schreiben
   // TODO: nachsehen ob TX_STAMP automatisch geschrieben wird
-  execute_nonreturning_SPI_command(CMD_TX);
+  dwt_writefastCMD(CMD_TX);
 }
