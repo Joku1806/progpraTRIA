bc86fad82e1984aef8a456a23f1ef871c8917beb
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-05 00:20:49 +0100

    Verschiedene Verbesserungen am DW3000 Interface:
    1. Ausbau von handle_incoming_packet()
    2. VERIFY wird jetzt überall benutzt, um potentielle falsche Benutzung der Funktionen und andere Fehler abzufangen
    3. Cachen von Buffern/Timestamps/Paketen, um so wenig Speicher in handle_incoming_packet alloziieren wie möglich
    4. Benutzung der verbesserten Field/Packet Interfaces
    5. Zusätzliche kleinere Bugfixes
    
    Natürlich ist jetzt noch nicht alles fertig und es bleiben noch ein paar große Fragen offen,
    aber das ist ein großer Schritt zur Fertigstellung des Interfaces.

--- include/lib/DW3000_interface.h
+++ include/lib/DW3000_interface.h
@@ -1,21 +1,32 @@
 #pragma once
 
-#include <optional>
-#include <lib/pin_layout.h>
-#include <lib/TRIA_Packet.h>
-#include <lib/DW3000_DECA_API/Src/decadriver/deca_device_api.h>
 #include <SPI.h>
+#include <lib/DW3000_DECA_API/Src/decadriver/deca_device_api.h>
+#include <lib/packets/TRIA_GenericPacket.h>
+#include <lib/packets/TRIA_RangeReport.h>
+#include <lib/packets/TRIA_RangeRequest.h>
+#include <lib/packets/TRIA_RangeResponse.h>
+#include <lib/pin_layout.h>
+#include <optional>
 
 class DW3000_Interface {
-  public:
-    DW3000_Interface(TRIA_ID& id, void (*recv_handler)(const dwt_cb_data_t *cb_data));
+public:
+  DW3000_Interface(TRIA_ID &id, void (*recv_handler)(const dwt_cb_data_t *cb_data));
 
-    std::optional<TRIA_Packet> handle_incoming_packet(size_t received_bytes);
-    void send_packet(TRIA_Packet& packet);
-  private:
-    TRIA_ID m_id;
-    TRIA_Stamp m_saved_tx;
-    uint8_t rx_buffer[TRIA_Packet::MAX_PACKET_SIZE];
-    SPISettings m_spi_settings;
-    void reset_DW3000();
+  std::optional<TRIA_RangeReport> handle_incoming_packet(size_t received_bytes);
+  void send_packet(TRIA_GenericPacket &packet);
+
+private:
+  SPISettings m_spi_settings;
+  TRIA_ID m_id;
+
+  uint8_t m_packet_buffer[TRIA_GenericPacket::PACKED_SIZE];
+  uint8_t m_stamp_buffer[TRIA_Stamp::PACKED_SIZE];
+
+  TRIA_Stamp m_rx_stamp;
+  TRIA_Stamp m_tx_stamp;
+  TRIA_RangeRequest m_cached_range_request;
+  TRIA_RangeResponse m_cached_range_response;
+
+  void reset_DW3000();
 };
--- lib/DW3000_interface.cpp
+++ lib/DW3000_interface.cpp
@@ -1,8 +1,9 @@
-#include <lib/DW3000_interface.h>
-#include <lib/DW3000_DECA_API/Src/decadriver/deca_regs.h>
 #include <Arduino.h>
+#include <lib/DW3000_DECA_API/Src/decadriver/deca_regs.h>
+#include <lib/DW3000_interface.h>
+#include <lib/assertions.h>
 
-DW3000_Interface::DW3000_Interface(TRIA_ID& id, void (*recv_handler)(const dwt_cb_data_t *cb_data)) {
+DW3000_Interface::DW3000_Interface(TRIA_ID &id, void (*recv_handler)(const dwt_cb_data_t *cb_data)) {
   m_id = id;
   m_spi_settings = SPISettings(10000000, MSBFIRST, SPI_MODE0);
   reset_DW3000();
@@ -11,19 +12,19 @@ DW3000_Interface::DW3000_Interface(TRIA_ID& id, void (*recv_handler)(const dwt_c
   // TODO: nachfragen ob diese Werte in Ordnung sind,
   // bzw. ob der SPI Speed runtergeschraubt werden muss
   dwt_config_t config = {
-    .chan = 5,
-    .txPreambLength = DWT_PLEN_32,
-    .rxPAC = DWT_PAC4,
-    .txCode = 3,
-    .rxCode = 3,
-    .sfdType = DWT_SFD_DW_16,
-    .dataRate = DWT_BR_6M8,
-    .phrMode = DWT_PHRMODE_STD,
-    .phrRate = DWT_BR_6M8,
-    .sfdTO = DWT_PLEN_32 + 1 + DWT_SFD_DW_16 - DWT_PAC4,
-    .stsMode = DWT_STS_MODE_OFF,
-    .stsLength = DWT_STS_LEN_32,
-    .pdoaMode = DWT_PDOA_M0,
+      .chan = 5,
+      .txPreambLength = DWT_PLEN_32,
+      .rxPAC = DWT_PAC4,
+      .txCode = 3,
+      .rxCode = 3,
+      .sfdType = DWT_SFD_DW_16,
+      .dataRate = DWT_BR_6M8,
+      .phrMode = DWT_PHRMODE_STD,
+      .phrRate = DWT_BR_6M8,
+      .sfdTO = DWT_PLEN_32 + 1 + DWT_SFD_DW_16 - DWT_PAC4,
+      .stsMode = DWT_STS_MODE_OFF,
+      .stsLength = DWT_STS_LEN_32,
+      .pdoaMode = DWT_PDOA_M0,
   };
 
   dwt_configure(&config);
@@ -40,47 +41,65 @@ void DW3000_Interface::reset_DW3000() {
   delayMicroseconds(1000);
 }
 
-std::optional<TRIA_Packet> DW3000_Interface::handle_incoming_packet(size_t received_bytes) {
-  if (received_bytes > TRIA_Packet::MAX_PACKET_SIZE + FCS_LEN) {
-    // TODO: Fehlermeldung ausgeben
+std::optional<TRIA_RangeReport> DW3000_Interface::handle_incoming_packet(size_t received_bytes) {
+  VERIFY(received_bytes - FCS_LEN <= TRIA_GenericPacket::PACKED_SIZE);
+  dwt_readrxdata(m_packet_buffer, received_bytes - FCS_LEN, 0);
+
+  TRIA_Action a;
+  a.initialise_from_buffer(m_packet_buffer);
+  VERIFY(a.value() != range_report);
+
+  switch (a.value()) {
+    case range_request: VERIFY(received_bytes - FCS_LEN == TRIA_RangeRequest::PACKED_SIZE);
+    case range_response: VERIFY(received_bytes - FCS_LEN == TRIA_RangeResponse::PACKED_SIZE);
+  }
+
+  TRIA_ID receive_mask;
+  receive_mask.initialise_from_buffer(m_packet_buffer + TRIA_Action::PACKED_SIZE + TRIA_ID::PACKED_SIZE);
+  if (!m_id.matches_mask(receive_mask)) {
+    dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXFCG_BIT_MASK);
     return {};
   }
 
-  dwt_readrxdata(rx_buffer, received_bytes - FCS_LEN, 0);
+  dwt_readrxtimestamp(m_stamp_buffer);
+  m_rx_stamp.initialise_from_buffer(m_stamp_buffer);
+
+  TRIA_GenericPacket *received;
+  switch (a.value()) {
+    case range_request: received = &m_cached_range_request;
+    case range_response: received = &m_cached_range_response;
+  }
+
+  received->initialise_from_buffer(m_packet_buffer);
   dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXFCG_BIT_MASK);
 
-  TRIA_Packet received;
-  received.initialise_from_buffer(rx_buffer);
-
-  if (!received.is_addressed_to(m_id)) {
-    return {};
-  }
-
-  TRIA_Stamp rx_stamp;
-  dwt_readrxtimestamp(rx_stamp.packed());
-
-  if (received.is_type(range_request)) {
-    TRIA_Packet response = TRIA_Packet(TRIA_Action(range_response), received.get_sender_id(), m_id, rx_stamp);
+  if (received->is_type(range_request)) {
+    auto response = TRIA_RangeResponse(received->received_from(), m_id, m_rx_stamp);
     send_packet(response);
-  } else if (received.is_type(range_response)) {
-    rx_stamp = rx_stamp - (received.get_tx_stamp() - received.get_rx_stamp());
-    return TRIA_Packet(TRIA_Action(range_report), received.get_sender_id(), m_id, rx_stamp, m_saved_tx);
+    return {};
+  } else {
+    // FIXME: irgendwie ohne hässlichen Cast hinkriegen
+    TRIA_Stamp measured_rx = ((TRIA_RangeResponse *)received)->get_rx_stamp();
+    TRIA_Stamp measured_tx = ((TRIA_RangeResponse *)received)->get_tx_stamp();
+    m_rx_stamp = m_rx_stamp - (measured_tx - measured_rx);
+    return TRIA_RangeReport(received->received_from(), m_id, m_rx_stamp, m_tx_stamp);
   }
-
-  return {};
 }
 
-void DW3000_Interface::send_packet(TRIA_Packet& packet) {
+void DW3000_Interface::send_packet(TRIA_GenericPacket &packet) {
+  VERIFY(!packet->is_type(range_report));
   while (!(dwt_read32bitreg(SYS_STATUS_ID) & SYS_STATUS_TXFRS_BIT_MASK)) {}
-  dwt_writetxdata(packet.packed_size(), packet.packed(), 0);
-  dwt_writetxfctrl(packet.packed_size() + FCS_LEN, 0, 0);  
+
+  packet.pack_into(m_packet_buffer);
+  dwt_writetxdata(packet.packed_size(), m_packet_buffer, 0);
+  dwt_writetxfctrl(packet.packed_size() + FCS_LEN, 0, 0);
   // TODO: nachsehen ob TX_STAMP automatisch geschrieben wird
   dwt_writefastCMD(CMD_TX);
 
   if (packet.is_type(range_request)) {
-    // diesen Hack vielleicht noch mal umschreiben :^)
-    // sollte aber perfekt funktionieren, jedenfalls
-    // bis die Signatur von packed() geändert wird
-    dwt_readtxtimestamp(m_saved_tx.packed());
+    dwt_readtxtimestamp(m_stamp_buffer);
+    m_tx_stamp.initialise_from_buffer(m_stamp_buffer);
   }
+
+  dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_TXFRS_BIT_MASK);
 }
