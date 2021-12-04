12b0d7b90aa1abeee89bb58481b6f1ca22a86c2d
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-05 00:16:28 +0100

    Anpassungen an die neue Paketstruktur.

--- src/Tracker.cpp
+++ src/Tracker.cpp
@@ -4,30 +4,29 @@ DW3000_Interface interface;
 TRIA_ID id;
 
 static void receive_handler(const dwt_cb_data_t *cb_data) {
-  std::optional<TRIA_Packet> ret = interface.handle_incoming_packet(cb_data->datalength);
+  std::optional<TRIA_RangeReport> ret = interface.handle_incoming_packet(cb_data->datalength);
   if (!ret) {
     return;
   }
 
-  TRIA_Packet range_data = ret.value();
+  TRIA_RangeReport range_data = ret.value();
   range_data.print();
 }
 
 void setup() {
   interface = DW3000_Interface(id, receive_handler);
 
-  #ifdef SENDER
+#ifdef SENDER
   TRIA_ID id = TRIA_ID(tracker, 1);
-  #else
+#else
   TRIA_ID id = TRIA_ID(tracker, 2);
-  #endif
+#endif
 }
 
 void loop() {
-  #ifdef SENDER
-  // FIXME: Noch kein funktionierender constructor
-  TRIA_Packet t = TRIA_Packet(TRIA_Action(range_request), id, TRIA_ID(tracker, 0));
+#ifdef SENDER
+  auto t = TRIA_RangeRequest(id, TRIA_ID(tracker, 0));
   interface.send_packet(t);
   delay(2000);
-  #endif
+#endif
 }
