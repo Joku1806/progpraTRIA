3a32202efb630aa1b7b42972e2820aa92e05b97e
Jonas Kuß <jonaskuss@protonmail.com>
2022-01-03 14:33:00 +0100

    Funktionen nicht inline machen, weil sonst der Compiler denkt, dass sie nicht definiert sind. Vllt wegen fehlenden Deklarationen?

--- lib/DW3000_DECA_Driver/platform/deca_mutex.c
+++ lib/DW3000_DECA_Driver/platform/deca_mutex.c
@@ -2,17 +2,17 @@
 #include <decadriver/deca_device_api.h>
 #include <platform/pin_mappings.h>
 
-inline void disable_IRQ_interrupts() {
+void disable_IRQ_interrupts() {
   asm volatile("cpsid i" ::
                    : "memory");
 }
 
-inline void enable_IRQ_interrupts() {
+void enable_IRQ_interrupts() {
   asm volatile("cpsie i" ::
                    : "memory");
 }
 
-inline uint32_t get_interrupt_pin_status() {
+decaIrqStatus_t get_interrupt_pin_status() {
   return digitalRead(SPI_interrupt);
 }
 
