a5dc9f268ab56bfaeeadf350116d9951571b782d
Jonas Kuß <jonaskuss@protonmail.com>
2022-01-03 14:36:02 +0100

    Constructor löschen, da es keinen Sinn macht, ein Interface zu initialisieren. Dafür stattdessen einen Default virtuellen Destructor definieren, damit Subklassen diesen aufrufen können, wenn sie out of scope gehen.

--- include/lib/TRIA_Protocol/fields/TRIA_Field.h
+++ include/lib/TRIA_Protocol/fields/TRIA_Field.h
@@ -6,7 +6,7 @@
 
 class TRIA_Field {
 public:
-  TRIA_Field() {};
+  virtual ~TRIA_Field() {};
   virtual size_t pack_into(uint8_t *buffer) = 0;
   virtual size_t packed_size() = 0;
   virtual void initialise_from_buffer(uint8_t *buffer) = 0;
