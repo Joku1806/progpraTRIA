5d2cd72ddda2c418ba1a9f5e6d663eed0d507dcd
Jonas Kuß <jonaskuss@protonmail.com>
2022-01-05 13:55:25 +0100

    Byteorder Fix für Konvertierung Network-Byteorder <=> Host-Byteorder

--- lib/TRIA_Protocol/fields/TRIA_Stamp.cpp
+++ lib/TRIA_Protocol/fields/TRIA_Stamp.cpp
@@ -24,6 +24,7 @@ size_t TRIA_Stamp::pack_into(uint8_t *bytes) {
 #else
   uint64_t stamp_nb = __builtin_bswap64(m_stamp);
 #endif
+  stamp_nb <<= (sizeof(stamp_nb) - PACKED_SIZE) * 8;
   memcpy(bytes, &stamp_nb, PACKED_SIZE);
   return PACKED_SIZE;
 }
@@ -33,6 +34,7 @@ size_t TRIA_Stamp::packed_size() { return PACKED_SIZE; }
 void TRIA_Stamp::initialise_from_buffer(uint8_t *buffer) {
   uint64_t stamp_nb = 0;
   memcpy(&stamp_nb, buffer, PACKED_SIZE);
+  stamp_nb >>= (sizeof(stamp_nb) - PACKED_SIZE) * 8;
 
 #if __BIG_ENDIAN__
   m_stamp = stamp_nb;
