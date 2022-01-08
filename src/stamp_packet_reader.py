bf0ae8b9a583f813cf6785b799a51a6e84f44580
octl <duohex06@gmail.com>
2022-01-08 17:38:35 +0100

    this somewhat works now

--- src/stamp_packet_reader.py
+++ src/stamp_packet_reader.py
@@ -3,6 +3,17 @@ import serial
 from typing import Union
 from dataclasses import dataclass
 
+"""
+wants to be send things in this order
+while (Serial.read() != 0xff);
+  Serial.write(1);
+  
+  send((uint8_t *)&(dummy.rx),8);
+  send((uint8_t *)&(dummy.tx),8);
+  send((uint8_t *)&(dummy.aID),1);
+  send((uint8_t *)&(dummy.bID),1);
+  delay(100);
+"""
 
 @dataclass
 class StampPacket:
@@ -19,24 +30,23 @@ class StampPacket:
 class StampPacketReader:
     # prüft erst, ob der Port übereinstimmt
     SERIAL_PORT = '/dev/ttyACM0'
-    SEND_SIGNAL = 0xff
+    SEND_SIGNAL = 0xff.to_bytes(1,"little")
     PACKET_SIZE = 18
 
     def __init__(self):
         self.informant = serial.Serial('/dev/ttyACM0', 9600)
 
-    def receive(self) -> Union[StampPacket, None]:
+    def receive(self):# -> Union[StampPacket, None]: #this function alrays returns a list, maybe an empty one
         self.informant.write(self.SEND_SIGNAL)
-        count = self.informant.read(1)
+        count = int.from_bytes(self.informant.read(1),byteorder='little')
         packets = []
-
+        print(count)
         for i in range(count):
-          bytes = self.informant.read(self.PACKET_SIZE)
-          if not len(bytes) == self.PACKET_SIZE:
-            continue
+          bytys = self.informant.read(self.PACKET_SIZE)
+          #if not len(bytys) == self.PACKET_SIZE:continue should ALLWAYS be True
           (timestamp_start,
-            timestamp_stop, sender_devid, receiver_devid) = struct.unpack("QQBB", bytes)
-          packets.push(StampPacket(sender_devid, receiver_devid, timestamp_start, timestamp_stop))
+            timestamp_stop, sender_devid, receiver_devid) = struct.unpack("QQBB", bytys)
+          packets.append(StampPacket(sender_devid, receiver_devid, timestamp_start, timestamp_stop))
 
         return packets
 
@@ -47,5 +57,5 @@ if __name__ == "__main__":
     if packet == None:
       print("Nichts bekommen!")
       continue
-
+    packet = packet[0]
     print(f"ID1 = {packet.sender_devid}, ID2 = {packet.receiver_devid}, start = {hex(packet.timestamp_start)}, stop = {hex(packet.timestamp_stop)}")
