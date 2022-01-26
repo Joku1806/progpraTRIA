8e34add4beef11ab5ded8edc1f1001903bfe0436
Jonas Kuß <jonaskuss@protonmail.com>
2022-01-26 12:46:41 +0100

    Randfälle Overflow und Messungenauigkeiten in der Zeitdifferenz Berechnung abfangen.

--- src/stamp_packet_reader.py
+++ src/stamp_packet_reader.py
@@ -5,6 +5,7 @@ class StampPacketReader:
     SERIAL_PORT = '/dev/ttyACM0'
     SEND_SIGNAL = 0xff.to_bytes(1,"little")
     PACKET_SIZE = 19
+    MAX_MEASURE_TIMEDIFF = 0x6819f
 
     def __init__(self):
         self.informant = serial.Serial(self.SERIAL_PORT, 9600)
@@ -18,17 +19,26 @@ class StampPacketReader:
             measure_packed = self.informant.read(self.PACKET_SIZE, byteorder = 'little')
             (_, sendid, recvid, rxtime, txtime) = struct.unpack(">BBBQQ", measure_packed)
             
-            # Gesammelte Zeitstempel sind in nativer Clock-Zeit, deswegen
+            timediff = 0
+            if rxttime >= txtime:
+                timediff = rxtime - txtime
+            else if txtime - rxtime > MAX_MEASURE_TIMEDIFF:
+                # Timer Overflow während der Messung, Prüfwert wurde durch
+                # maximale Distanz von 2km mit (2000m / c_air) / 15.65 * 1000000000000
+                # berechnet.
+                timediff = 0xffffffffff - txtime + rxtime
+            else:
+                # Sehr kleine Distanzen können durch Ungenauigkeiten in der Messung
+                # negativ werden, in dem Fall einfach von 0 ausgehen.
+                timediff = 0
+
+            # Berechnete Zeitdifferenz ist in nativer Clock-Zeit, deswegen
             # erst in Picosekunden und dann in Sekunden umrechnen.
-            # Es kann sein, dass wegen "Rauschen" die ToF negativ ist, das ist
-            # natürlich nicht möglich, deswegen gehen wir in dem Fall von 0 aus.
-            # FIXME: Deckt im Moment nicht den Fall ab, dass der Timer während
-            # der Messung überläuft.
-            ToF_s = max(rxtime - txtime, 0) * 15.65 / 1000000000000
+            ToF_s = timediff * 15.65 / 1000000000000
             measures.append({"id": sendid, 'tof': ToF_s})
 
         wrapped = {"mcus": measures}
-        return json.dumps(wrapped, indent=4)
+        return json.dumps(wrapped, indent = 4)
 
 if __name__ == "__main__":
     reader = StampPacketReader()
