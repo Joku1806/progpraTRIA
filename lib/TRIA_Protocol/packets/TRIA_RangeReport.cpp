7c0fccb619b8c2b10de10d12d40b50d372c50c8b
Jonas Kuß <jonaskuss@protonmail.com>
2022-01-27 12:20:22 +0100

    Lichtgeschwindigkeit (Luft) von Wolfram Alpha benutzen.

--- lib/TRIA_Protocol/packets/TRIA_RangeReport.cpp
+++ lib/TRIA_Protocol/packets/TRIA_RangeReport.cpp
@@ -6,24 +6,24 @@ void TRIA_RangeReport::print() {
   Serial.print("\n");
 
   // für Timer Zeug siehe User Manual S. 21
-  // c_air = 299702458
+  // c_air = 299709000
   // ToF_s = (RX - TX) * 15.65 picoseconds  / 1000000000000
   // distance = (ToF_s / 2) * c_air;
   TRIA_Stamp dt = get_rx_stamp() - get_tx_stamp();
-  double distance = dt.value() * 0.00234517173385;
+  double distance = dt.value() * 0.002345222925;
 
   Serial.print("Distanz von ");
   received_from().print();
   Serial.print(" zu ");
   sent_to().print();
   Serial.print(" ist ");
-  Serial.print(distance);
+  Serial.print(distance, 10);
   Serial.print("m.\n");
 }
 
 void TRIA_RangeReport::print_distance() {
   TRIA_Stamp dt = get_rx_stamp() - get_tx_stamp();
-  double distance = dt.value() * 0.00234517173385;
+  double distance = dt.value() * 0.002345222925;
   Serial.print(distance, 10);
   Serial.print("\n");
 }
