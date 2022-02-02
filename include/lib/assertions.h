acea5f90ac2b934012acb5836b88c29405400616
Jonas Kuß <jonaskuss@protonmail.com>
2022-02-02 14:11:46 +0100

    BENCHMARK macro hinzufügen, um naja... einfacher Benchmarks machen zu können ;)

--- include/lib/assertions.h
+++ include/lib/assertions.h
@@ -1,9 +1,20 @@
-// Inspiriert von https://github.com/SerenityOS/serenity/blob/master/Kernel/Assertions.h
 #pragma once
 
 #ifdef DEBUG
 
 #include <Arduino.h>
+
+#define BENCHMARK(fn)                                                                              \
+  do {                                                                                             \
+    Serial.print("--- Beginning Benchmark for " #fn "\n");                                         \
+    unsigned long bench_start = micros();                                                          \
+    fn;                                                                                            \
+    unsigned long bench_stop = micros();                                                           \
+    Serial.print("--- Ended Benchmark for " #fn ", took ");                                        \
+    Serial.print(bench_start <= bench_stop ? bench_stop - bench_start                              \
+                                           : ULONG_MAX - bench_start + bench_stop);                \
+    Serial.print("us.\n");                                                                         \
+  } while (0)
 
 #define VERIFY(expression)                                                                         \
   do {                                                                                             \
@@ -17,6 +28,8 @@
   } while (0)
 
 #else
+
+#define BENCHMARK(fn) fn;
 
 #define VERIFY(expression)                                                                         \
   do {                                                                                             \
